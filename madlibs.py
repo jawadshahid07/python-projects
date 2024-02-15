179da37660ac11451cda36efc1ec43fc570cb5fc
jawadshahid07 <jawad.shahid70@outlook.com>
2024-02-15 23:04:18 +0500

    simple madlibs code

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+# in this project, i will make madlibs
+# madlib is essentially string concatenation
+
+name = input("Name: ")
+hobby = input("Hobby: ")
+model = input ("Role Model: ")
+interest = input("Interest: ")
+
+madlib = f"My name is {name} and my favourite hobby for my free time \
+is {hobby}, my role model whom I look up to is {model}, and my interest is in \
+{interest}"
+
+print(madlib)
